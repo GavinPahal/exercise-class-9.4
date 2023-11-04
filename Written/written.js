3e2544f0c4785680be0d5ccdcc0736a8103af6a9
GavinPahal <gpahal4@my.bcit.ca>
2023-11-03 21:32:54 -0700

    adding Written folder and files

---
+++ Written/written.js
@@ -0,0 +1,36 @@
+/**
+ * Arrays
+ * 
+ * always use camel case where needed
+ * 
+ * Make sure to write the question/instruction and then the answers below
+ * 
+ */
+
+/**
+ * Question 5 - concat two different arrays by using .concat() method
+ * 
+ *  
+ */
+
+/***
+ * Question 1
+ * initalise var to cat with an array vaules of mouse, leaches, cow
+ * initalise var to animals with array vaules of animals, sea creatures, farm animals
+ * create one array
+ * console log the one array
+ * 
+ */
+
+var catOne=["mouse", "leaches", "cow"];
+var catTwo=["animals", "seaCreatures", "farmAnimals" ];
+var totalCats= catOne.concat(catTwo);
+console.log(totalCats);
+
+
+
+/**
+ * Question 6 - create a 2 dimensions array that is 2-by-4. Grab some values out of this array to create a sentence using concatenation.
+ * 
+ *  
+ */
